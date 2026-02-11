3d64735cc22113c87c1a0b3a487853b18f57faa3
thrishulshetty027 <shettythrishul1@gmail.com>
2026-02-11 11:49:38 +0530

    add simple add() function for test generation

--- test.c
+++ test.c
@@ -349,8 +349,7 @@ int main()
 
 	return 0;
 }
-/* Added AI Agent to the workflow */
-/*Manual changes to Trigger AI */
-/*Trying a Cloud model */
-/* checking to see if the agent  works */
-/* Trigger AI agent test2 */
+
+int add(int a, int b) {
+    return a + b;
+}
