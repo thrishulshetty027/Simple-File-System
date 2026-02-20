d6d7226601ca3900b5c52152cab9b44a283eb13d
thrishulshetty027 <shettythrishul1@gmail.com>
2026-02-20 16:26:17 +0530

    new c

--- test.c
+++ test.c
@@ -530,3 +530,38 @@ diff = """
 
 output = generate_testcases(diff)
 print(output)
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[]) {
+    int start = 0;
+    int end = strlen(str) - 1;
+    char temp;
+
+    while (start < end) {
+        temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    reverseString(str);
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
