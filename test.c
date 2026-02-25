eb9e169911e283e718960333086edd44227b57dc
thrishulshetty027 <shettythrishul1@gmail.com>
2026-02-25 14:40:26 +0530

    t20

--- test.c
+++ test.c
@@ -778,3 +778,25 @@ int main() {
 
     return 0;
 }
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int length, i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    length = strlen(str);
+
+    printf("Reversed string: ");
+    for (i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
