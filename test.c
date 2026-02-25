54a1a1c3ea4219227f088c41cb3bf835467a5fcf
thrishulshetty027 <shettythrishul1@gmail.com>
2026-02-25 12:33:44 +0530

    nc

--- test.c
+++ test.c
@@ -745,3 +745,19 @@ int main() {
 
     return 0;
 }
+
+
+
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n <= 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num = 5;
+    printf("Factorial of %d is %d\n", num, factorial(num));
+    return 0;
+}
