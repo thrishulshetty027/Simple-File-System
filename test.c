f30765f8077aabd93e8e7c8479a58860b8a30201
thrishulshetty027 <shettythrishul1@gmail.com>
2026-02-25 15:42:00 +0530

    t21

--- test.c
+++ test.c
@@ -800,3 +800,25 @@ int main() {
 
     return 0;
 }
+
+
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 10, y = 20;
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("After swap: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
