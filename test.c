3f887909cac4b7ecce306699682701f2a4fff192
thrishulshetty027 <shettythrishul1@gmail.com>
2026-02-25 14:19:24 +0530

    nw

--- test.c
+++ test.c
@@ -761,3 +761,20 @@ int main() {
     printf("Factorial of %d is %d\n", num, factorial(num));
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("%d is Even\n", num);
+    else
+        printf("%d is Odd\n", num);
+
+    return 0;
+}
