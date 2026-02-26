60ae905896710203d71fd5b6c857a9226f91885a
thrishulshetty027 <shettythrishul1@gmail.com>
2026-02-26 20:50:44 +0530

    test27

--- test.c
+++ test.c
@@ -1229,4 +1229,21 @@ int main() {
     return 0;
 }
 
+#include <stdio.h>
+
+// Function to calculate factorial
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num = 5;
+
+    printf("Factorial of %d is %d\n", num, factorial(num));
+
+    return 0;
+}
+
 
