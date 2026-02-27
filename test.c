62971fee96a729ea2f39700beaa544b55ade9906
thrishulshetty027 <shettythrishul1@gmail.com>
2026-02-27 11:25:15 +0530

    newpr

--- test.c
+++ test.c
@@ -1283,3 +1283,38 @@ int main() {
 }
 
 
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int number, originalNumber, remainder, n = 0;
+    double result = 0.0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    originalNumber = number;
+
+    // Count number of digits
+    while (originalNumber != 0) {
+        originalNumber /= 10;
+        ++n;
+    }
+
+    originalNumber = number;
+
+    // Calculate sum of powers
+    while (originalNumber != 0) {
+        remainder = originalNumber % 10;
+        result += pow(remainder, n);
+        originalNumber /= 10;
+    }
+
+    if ((int)result == number)
+        printf("%d is an Armstrong number.\n", number);
+    else
+        printf("%d is not an Armstrong number.\n", number);
+
+    return 0;
+}
