37b4d3aa6a3d0cdd19a527b981e795aab7dfed35
thrishulshetty027 <shettythrishul1@gmail.com>
2026-03-09 14:32:46 +0530

    acrtest20

--- test.c
+++ test.c
@@ -1185,3 +1185,22 @@ int* merge_sorted_arrays(int* a, int sizeA, int* b, int sizeB)
 
     return result;
 }
+
+int removeDuplicates(int arr[], int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+
+    int j = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] != arr[j])
+        {
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+
+    return j + 1;
+}
