66303ef1e693021732631a135ea7fa658f07cff7
thrishulshetty027 <shettythrishul1@gmail.com>
2026-03-09 16:32:57 +0530

    acrtest22

--- test.c
+++ test.c
@@ -1237,3 +1237,31 @@ int longestCommonPrefix(char **strs, int count, char *result)
     result[index] = '\0';
     return index;
 }
+
+
+#include <string.h>
+
+int areAnagrams(const char *str1, const char *str2)
+{
+    int count[256] = {0};
+
+    while (*str1)
+    {
+        count[(unsigned char)*str1]++;
+        str1++;
+    }
+
+    while (*str2)
+    {
+        count[(unsigned char)*str2]--;
+        str2++;
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        if (count[i] != 0)
+            return 0;
+    }
+
+    return 1;
+}
