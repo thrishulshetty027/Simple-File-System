6e88feede0873ca3c97fd16711e6b2365beea2f2
thrishulshetty027 <shettythrishul1@gmail.com>
2026-03-13 11:10:28 +0530

    test.c

--- test.c
+++ test.c
@@ -1634,3 +1634,19 @@ void freeList(Node *head)
         free(temp);
     }
 }
+
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int age;
+};
+
+int main() {
+    struct Student s1 = {"Alice", 20};
+
+    printf("Name: %s\n", s1.name);
+    printf("Age: %d\n", s1.age);
+
+    return 0;
+}
