24bb4b99aef1942987891b0d8710d1443f23c158
thrishulshetty027 <shettythrishul1@gmail.com>
2026-03-13 15:23:26 +0530

    test.c

--- test.c
+++ test.c
@@ -1741,3 +1741,19 @@ int main() {
 
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    int a = 24, b = 36, gcd;
+
+    for (int i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0) {
+            gcd = i;
+        }
+    }
+
+    printf("GCD of %d and %d is %d\n", a, b, gcd);
+
+    return 0;
+}
