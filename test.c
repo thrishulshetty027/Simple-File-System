041561ae12fc05bc71a7c6b9e02f68b7911f7ef7
thrishulshetty027 <shettythrishul1@gmail.com>
2026-03-13 15:40:50 +0530

    test.c

--- test.c
+++ test.c
@@ -1757,3 +1757,16 @@ int main() {
 
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, c = 30;
+    float average;
+
+    average = (a + b + c) / 3.0;
+
+    printf("Average: %.2f\n", average);
+
+    return 0;
+}
