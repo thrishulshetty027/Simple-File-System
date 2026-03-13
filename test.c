e4f222269c6a755e5c6f02f757810ba5418038e0
thrishulshetty027 <shettythrishul1@gmail.com>
2026-03-13 12:27:45 +0530

    test.c

--- test.c
+++ test.c
@@ -1688,3 +1688,24 @@ int main() {
 
     return 0;
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int num = 29, i, flag = 0;
+
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("%d is a Prime number\n", num);
+    else
+        printf("%d is not a Prime number\n", num);
+
+    return 0;
+}
